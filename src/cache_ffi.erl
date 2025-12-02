8d9bf03787f12cdf4c12d546fb98e153c31136b6
OtsoHellman <otso.hellman@gmail.com>
2025-12-02 09:55:14 +0200

    cleanups

--- src/cache_ffi.erl
+++
@@ -1,4 +0,0 @@
--module(cache_ffi).
--export([identity/1]).
-
-identity(X) -> X.
--- src/ffi.erl
+++
@@ -1,6 +0,0 @@
--module(ffi).
--export([named_table/0, public_table/0, set_table/0]).
-
-named_table() -> named_table.
-public_table() -> public.
-set_table() -> set.
